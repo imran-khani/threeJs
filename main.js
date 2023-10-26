e21b3055886e2ef9b994d23a40a8e702f30f47d2
imran khan <imranloveh01@gmail.com>
2023-10-26 16:35:09 +0500

    learing three js

--- main.js
+++ main.js
@@ -2,17 +2,46 @@ import "./style.css";
 
 import * as THREE from "three";
 
-// scene
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 2, 5);
 
-// create sphere
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
 
-const geometry = new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshStandardMaterial({
-  color: "#00ff83",
-});
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
 
-const mesh = new THREE.Mesh(geometry, material);
 
-scene.add(mesh);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+renderer.render(scene, camera);
